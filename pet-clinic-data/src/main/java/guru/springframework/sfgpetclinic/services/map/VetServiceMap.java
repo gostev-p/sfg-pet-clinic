4e9f5946d72953a02633d99b174293607a14f60c
Gostev, Philipp <philipp.gostev@adesso.de>
2023-03-09 15:12:00 +0100

    fixing broken links

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,7 +2,6 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.model.Vet;
-import guru.springframework.sfgpetclinic.services.CrudService;
 import guru.springframework.sfgpetclinic.services.SpecialityService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
@@ -37,9 +36,9 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
         if (object.getSpecialties().size() > 0){
             object.getSpecialties().forEach(speciality -> {
-                if (speciality.getId() == null) {
-                    Speciality savedSpeciality = specialityService.save(speciality);
-                    speciality.setId(savedSpeciality.getId());
+                if(speciality.getId() == null){
+                    Speciality savedSpecialty = specialityService.save(speciality);
+                    speciality.setId(savedSpecialty.getId());
                 }
             });
         }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
 
         return "index";
     }
+
+    @RequestMapping("/oups")
+    public String oupsHandler() {
+        return "notimplemented";
+    }
 }
