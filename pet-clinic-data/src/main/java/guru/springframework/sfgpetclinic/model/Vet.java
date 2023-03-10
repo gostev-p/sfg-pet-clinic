485b176395df4d080adf68dba825c8830e836e66
Gostev, Philipp <philipp.gostev@adesso.de>
2023-03-10 16:31:54 +0100

    refactoring to lombok project

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -13,12 +19,4 @@ public class Vet extends Person{
             joinColumns = @JoinColumn(name = "vet_id"),
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialties = new HashSet<>();
-
-    public Set<Speciality> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Speciality> specialties) {
-        this.specialties = specialties;
-    }
 }
